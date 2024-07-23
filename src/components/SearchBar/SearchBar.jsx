3743aa3c25a3b7bc79c5055d7e1edf3aada18a19
88Sergiy <152657236+88Sergiy@users.noreply.github.com>
2024-07-23 18:50:55 +0300

    commit-3

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,38 +2,40 @@ import styles from './SearchBar.module.css';
 import PropTypes from 'prop-types';
 import toast from 'react-hot-toast';
 
-function SearchBar({ onSumbit }) {
-    function submitHandler(e) {
-        e.preventDefault();
-        const { search } = e.target.elements;
+function SearchBar({ onSubmit }) {
+  function submitHandler(e) {
+    e.preventDefault();
+    const { search } = e.target.elements;
 
-        const searchString = search.value.trim();
+    const searchString = search.value.trim();
 
-        if (!searchString) {
-            return toast('You query shouldn`t be empty', {
-                duration: 3000,
-                position: 'top-right',
-                icon: '⚠️'
-            });
-        }
-        onSumbit(searchString);
+    if (!searchString) {
+      return toast('Your query shouldn`t be empty', {
+        duration: 3000,
+        position: 'top-right',
+        icon: '⚠️',
+      });
     }
+
+    onSubmit(searchString);
+  }
     return (
-        <header className={styles.header}>
-            <from onSumbit={sumbitHandler} className={styles.from}>
-                <input
-                    className={FileSystemWritableFileStream.input}
-                    type="text"
-                    autoComplete="of"
-                    autoFocusplaceholder="Search smages and photos"
-                    name="search"
-                />
-                <button className={styles.btn} type="sumbit">
-                    Search
-                </button>
-            </from>
-        </header>
-    );
+    <header className={styles.header}>
+      <form onSubmit={submitHandler} className={styles.form}>
+        <input
+          className={styles.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="search"
+        />
+        <button className={styles.btn} type="submit">
+          Search
+        </button>
+      </form>
+    </header>
+  );
 }
 
 SearchBar.propTypes = {
